004dea38a2db0faca8d65f17921f3ed6e8006c8f
Mpenni <104714643+Mpenni@users.noreply.github.com>
2023-10-19 22:35:16 +0200

    fix tracks contraints

---
+++ DigitalHunt/Models/APImanager2.swift
@@ -0,0 +1,8 @@
+//
+//  APImanager2.swift
+//  DigitalHunt
+//
+//  Created by Dave Stops on 19/10/23.
+//
+
+import Foundation
---
+++ DigitalHunt/Models/Node.swift
@@ -0,0 +1,8 @@
+//
+//  Node.swift
+//  DigitalHunt
+//
+//  Created by Dave Stops on 19/10/23.
+//
+
+import Foundation
